b9f68abdb9e38c1216a6d1dcef4c790520763f61
Stanley Cao <daydream2013@live.com>
2018-04-23 01:14:22 +0800

    add install script

---
+++ app/Console/Commands/Install.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Users;
+use Illuminate\Console\Command;
+
+class Install extends Command
+{
+    protected $signature = 'install';
+
+    protected $description = 'Install Typist API';
+
+    public function handle()
+    {
+        $email = $this->ask('email');
+        $username = $this->ask('Username');
+        $password = $this->secret('Password');
+
+        $user = [
+            'name' => $username,
+            'password' => Hash::make($password),
+            'email' => $email
+        ];
+
+        Users::create($user);
+        
+        echo 'completed';
+    }
+}
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,25 +2,61 @@
 
 namespace App\Http\Controllers;
 use App\Posts;
-use App\FragmentComments;
-use App\PostComments;
+use App\Comments;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function createCommentByPostId(int $id)
+    public function createCommentByPostId(int $id, Request $request)
     {
+        $post = Posts::find($id);
+        if ($post)
+        {
+            $comment = [
+                'commenter_ip' => $request->ip(),
+                'content' => $request->content
+            ];
+
+            $comments = $post->comments()->create($comment);
+
+            return response()->json($comments);
+        }
+
+        abort(404, 'Post Not Found');
     }
 
-    public function retriveCommentsByPostId(int $id, int $page)
+    public function deleteComment(int $id)
     {
+        $comment = Comments::find($id);
+        if ($comment)
+        {
+            $comment->delete();
+            return response()->json(['message' => 'deleted']);
+        }
+
+        abort(404, 'Comment not found');
     }
 
-    public function deleteCommentByPostId(int $id)
+    public function retriveComments(int $page, int $id)
     {
-    }
+        $offset = ($page - 1) * 10;
+        if ($id)
+        {
+            $post = Posts::find($id);
+            if ($post)
+            {
+                $comments = $post->comments();
+                $count = $comments->count();
+                $comments = $comments->orderBy('created_at', 'desc')
+                             ->skip($offset)->take(10)->get();
+            } else {
+                abort(404, 'Post not found');
+            }
+        } else {
+            $count = Comment::count();
+            $comments = Comments::skip($offset)->take(10)->get();
+        }
 
-    public function deleteCommentsByPostId(int $id)
-    {
+        return response()->json(['count' => $count, 'comments' => $comments]);
     }
 }
