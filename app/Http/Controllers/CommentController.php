414917ffbb35ca705621a7ed707760f8f67fae79
Stanley Cao <daydream2013@live.com>
2018-04-23 17:48:41 +0800

    optimise date format and location

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,6 +8,17 @@ use GeoIp2\Database\Reader;
 
 class CommentController extends Controller
 {
+	private static function getLocation(string $ip)
+	{
+		$reader = new Reader('../database/GeoLite2-City.mmdb');
+		try {
+			$record = $reader->city($ip);
+		} catch (\GeoIp2\Exception\AddressNotFoundException $e) {
+		}
+
+		return isset($record) ? ($record->city->name + ', ' + $record->country->name) : 'Unknown';
+	}
+
     public function createCommentByPostId(int $id, Request $request)
     {
         $post = Posts::find($id);
@@ -19,6 +30,7 @@ class CommentController extends Controller
             ];
 
             $comments = $post->comments()->create($comment);
+			$comments->location = Self::getLocation($comments->commenter_ip);
 
             return response()->json($comments);
         }
@@ -46,22 +58,12 @@ class CommentController extends Controller
             $post = Posts::find($id);
             if ($post)
             {
-                $reader = new Reader('../database/GeoLite2-City.mmdb');
                 $comments = $post->comments();
                 $count = $comments->count();
                 $comments = $comments->orderBy('created_at', 'desc')
                              ->skip($offset)->take(10)->get();
                 foreach ($comments as $comment) {
-                    try {
-                        $record = $reader->city($comment->commenter_ip);
-                    } catch (\GeoIp2\Exception\AddressNotFoundException $e) {
-                    }
-
-                    if (isset($record)) {
-                        $comment->location = $record->city->name;
-                    } else {
-                        $comment->location = 'Unknown';
-                    }
+					$comment->location = Self::getLocation($comment->commenter_ip);
                 }
             } else {
                 abort(404, 'Post not found');
