f83b06b09a655a03ae61ffe40d45304206b9c7aa
Stanley Cao <daydream2013@live.com>
2018-06-07 15:09:59 +0800

    optimise RSS output

--- app/Http/Controllers/RSSController.php
+++ app/Http/Controllers/RSSController.php
@@ -24,11 +24,12 @@ class RSSController extends Controller
             $item = new Item();
             $item
             ->title($post->title ? $post->title : "Untitled")
-            ->description("No description available")
+            ->description($post->expand_content ? $post->revision->content : "No description available")
             ->url(getenv('FRONTEND_BASE_URL').'/post/'.$post->id)
             ->creator($post->revision->author->name)
             ->pubDate(strtotime($post->updated_at))
             ->category($post->category_id != 0 ? $post->category->name : 'Uncategorized')
+            ->content(htmlspecialchars(Markdown::defaultTransform($post->revision->content)))
             ->appendTo($channel);
         }
 
